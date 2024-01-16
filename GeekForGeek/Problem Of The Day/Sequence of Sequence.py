efc034e03ba20850aa1cf5494886d47fa256dc8b
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-01-16 22:27:07 +0530

    Create Sequence of Sequence.py

---
+++ GeekForGeek/Problem Of The Day/Sequence of Sequence.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+
+class Solution:
+    def numberSequence(self, m, n):
+        if m<n:
+            return 0
+        if n==0:
+            return 1
+        return self.numberSequence(m/2,n-1)+self.numberSequence(m-1,n)
