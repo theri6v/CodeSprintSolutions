ac1655617ee776f22c66323b3be3b0c902877691
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-02-13 21:00:56 +0530

    Create Clone an Undirected Graph.py

---
+++ GeekForGeek/Problem Of The Day/Clone an Undirected Graph.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, val = 0, neighbors = []):
+        self.val = val
+        self.neighbors = neighbors
+'''
+import sys
+sys.setrecursionlimit(10000)
+from collections import deque
+class Solution():
+    def cloneGraph(self, node):
+        visited=dict()
+        queue=deque()
+        queue.append(node)
+        visited[node]=Node(node.val,[])
+        while queue:
+            u=queue.popleft()
+            new_u=visited[u]
+            for v in u.neighbors:
+                if v not in visited:
+                    queue.append(v)
+                    visited[v]=Node(v.val,[])
+                new_u.neighbors.append(visited[v])
+        return visited[node]
+
