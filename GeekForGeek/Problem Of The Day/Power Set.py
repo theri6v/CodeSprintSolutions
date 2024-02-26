bd9603778ffd4990ee20d6b58da33fe0895cd1da
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-02-26 22:56:31 +0530

    Create Power Set.py

---
+++ GeekForGeek/Problem Of The Day/Power Set.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+
+class Solution:
+    def AllPossibleStrings(self, s):
+        # Code here
+        res = []
+        
+        def solve(st, i):
+            if i == len(s):
+                return
+            
+            res.append(st+s[i])
+            
+            solve(st+s[i], i+1)
+            solve(st, i+1)
+        
+        solve("", 0)
+        return sorted(res)
