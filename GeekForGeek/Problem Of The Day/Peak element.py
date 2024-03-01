e49c21d3d7094f383c2f14fa25141aa17a52511b
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-03-01 22:39:39 +0530

    Create Peak element.py

---
+++ GeekForGeek/Problem Of The Day/Peak element.py
@@ -0,0 +1,8 @@
+# your task is to complete this function
+# function should return index to the any valid peak element
+class Solution:   
+    def peakElement(self,arr, n):
+        # Code here
+        return arr.index(max(arr))
+
+
