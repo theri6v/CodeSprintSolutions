cd08d3245252c606964367f9f77d9f3271901fa3
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-03-14 23:33:16 +0530

    Create Largest subsquare surrounded by X.py

---
+++ GeekForGeek/Problem Of The Day/Largest subsquare surrounded by X.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def largestSubsquare(self, n, a):
+        #code here
+        n = len(a)
+        dp = [[(0, 0)]*(n+1) for _ in range(n+1)]
+        
+        for r in range(n):
+            for c in range(n):
+                if a[r][c] == 'X':
+                    dp[r+1][c+1] = (dp[r][c+1][0]+1, dp[r+1][c][1]+1)
+        
+
+        ans = 0
+        for r in range(1, n+1):
+            for c in range(1, n+1):
+                d = min(dp[r][c])
+                while d > ans:
+                    left = c-d+1
+                    top = r-d+1
+                    if dp[r][left][0] >= d and dp[top][c][1] >= d:
+                        ans = d
+                        break
+                    d -= 1
+        return ans
