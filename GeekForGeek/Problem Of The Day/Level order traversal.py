55d1fe0dac4bad7147e0167d0cc57636b2f43cdb
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-03-18 17:07:53 +0530

    Create Level order traversal.py

---
+++ GeekForGeek/Problem Of The Day/Level order traversal.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+class Solution:
+    def levelOrder(self,root):
+        queue = deque([root])
+        res = []
+        
+        while queue:
+            node = queue.popleft()
+            res.append(node.data)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            
+        return res
