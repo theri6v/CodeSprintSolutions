25a916bc541d290f523c862ad71b19be4293050a
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-04-07 13:31:43 +0530

    Create Maximize dot product.py

---
+++ GeekForGeek/Problem Of The Day/Maximize dot product.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+    def maxDotProduct(self, n, m, a, b):
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for j in range(1,m+1):
+            dp[0][j]=float('-inf')
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                dp[i][j]=max(dp[i-1][j],dp[i-1][j-1]+a[i-1]*b[j-1])
+        return dp[n][m]
