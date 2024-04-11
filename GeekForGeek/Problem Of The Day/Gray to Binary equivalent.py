4cde45f89e724a839af54f29151fc4dc756d54c9
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-04-11 13:10:43 +0530

    Create Gray to Binary equivalent.py

---
+++ GeekForGeek/Problem Of The Day/Gray to Binary equivalent.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+#easy solution
+
+class Solution:    
+    def grayToBinary(self,n):
+        bi=bin(n)#convert decimal to binary
+        s=str(bi)#convert binary to string
+        ans=""
+        for i in range(2,len(s)):
+            if i==2:
+                ans+=s[i]
+            else:
+                ans+=str(int(ans[-1])^int(s[i])) #taking xor with msb 
+        deci=int(ans,2)#convert binary str to decimal
+        return deci
