5c8c6f73a0e787b90ab6100f59294514911e0b36
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-04-15 22:41:37 +0530

    Create Sum Root to Leaf Numbers.py

---
+++ leetcode/Daily Coding Challenge/Sum Root to Leaf Numbers.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def sumNumbers(self, root: Optional[TreeNode]) -> int:
+    ans = 0
+
+    def dfs(root: Optional[TreeNode], path: int) -> None:
+      nonlocal ans
+      if not root:
+        return
+      if not root.left and not root.right:
+        ans += path * 10 + root.val
+        return
+
+      dfs(root.left, path * 10 + root.val)
+      dfs(root.right, path * 10 + root.val)
+
+    dfs(root, 0)
+    return ans
