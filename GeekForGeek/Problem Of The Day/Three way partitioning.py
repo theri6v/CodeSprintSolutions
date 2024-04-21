52f506ddb70cf95203660a9f2a667cd9e3278ddb
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-04-21 16:09:01 +0530

    Create Three way partitioning.py

---
+++ GeekForGeek/Problem Of The Day/Three way partitioning.py
@@ -0,0 +1,9 @@
+#User function template for Python
+
+class Solution:
+    #Function to partition the array around the range such 
+    #that array is divided into three parts.
+    def threeWayPartition(self, array, a, b):
+        a=array.sort()
+        return a
+        # code here 
