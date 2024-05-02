e5abd099ab47c5cbc3225db35c413ecd2a52ea9a
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-05-02 12:53:39 +0530

    Create Serialize and deserialize a binary tree.py

---
+++ GeekForGeek/Problem Of The Day/Serialize and deserialize a binary tree.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+
+'''
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    #Function to serialize a tree and return a list containing nodes of tree.
+    def serialize(self, root):
+        
+        def inorder(root , res):
+            if root:
+            
+                inorder(root.left , res)
+                res.append(root.data)
+                inorder(root.right ,res)
+        res = []
+        inorder(root ,res)
+        
+        return res
+        #code here
+    
+    #Function to deserialize a list and construct the tree.   
+    def deSerialize(self, a):
+        
+        
+        def helper(a, l , r):
+            if l > r:
+                return 
+            idx = (l+r)//2
+            
+            root = Node(a[idx])
+            
+            root.left = helper(a , l , idx-1)
+            root.right = helper(a, idx +1 , r)
+            return root 
+        return helper(a, 0 , len(a)-1)
+        #code here
+ 
