07e30236aa3f635653c358fdca958bea5b33053c
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-05-20 01:11:54 +0530

    Create Modular Exponentiation for large numbers.py

---
+++ GeekForGeek/Problem Of The Day/Modular Exponentiation for large numbers.py
@@ -0,0 +1,12 @@
+#User function Template for python3
+
+class Solution:
+    def PowMod(self, x, n, m):
+        # Code here
+        ans = 1
+        while n > 0:
+            if n&1 != 0:
+                ans = (ans*x)%m
+            x = x*x%m
+            n >>= 1
+        return ans
