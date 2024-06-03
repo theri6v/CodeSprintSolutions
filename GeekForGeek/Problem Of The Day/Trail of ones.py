37c8193f1cf90e2f7d62c4f549d735492358ddc7
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-06-03 12:48:54 +0530

    Create Trail of ones.py

---
+++ GeekForGeek/Problem Of The Day/Trail of ones.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+class Solution:
+    def numberOfConsecutiveOnes (ob,n):
+        a, b = 1, 1
+        
+        for _ in range(n):
+            a, b = b, (a+b) % 1000000007
+            
+        return (pow(2, n, 1000000007) - b) % 1000000007
