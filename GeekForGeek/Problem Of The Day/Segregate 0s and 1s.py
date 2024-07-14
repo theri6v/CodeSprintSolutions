269e88ef86cfecdf534997e3cc9d4b5841b9249c
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-07-14 21:29:48 +0530

    Create Segregate 0s and 1s.py

---
+++ GeekForGeek/Problem Of The Day/Segregate 0s and 1s.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    def segregate0and1(self, arr):
+        arr.sort(key = None, reverse = False)
+        return arr
+            
+    
+            
+       
