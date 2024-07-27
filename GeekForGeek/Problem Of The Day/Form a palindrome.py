838880a96f5459ee86356f7c538e2273cc1c595f
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-07-27 15:18:23 +0530

    Create Form a palindrome.py

---
+++ GeekForGeek/Problem Of The Day/Form a palindrome.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+class Solution:
+    def countMin(self, str):
+        s1 = Str
+        n = len(s1)
+        s2 = s1[::-1]
+        dp = [[0 for i in range(n+1)] for j in range(n+1)]
+
+        for i in range(n-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if s1[i] == s2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        return n - dp[0][0]
