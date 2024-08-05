11101c01648f204bb1689d2e7b69fd2a056111fb
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-08-05 23:12:28 +0530

    Create Bottom View of Binary Tree.py

---
+++ GeekForGeek/Problem Of The Day/Bottom View of Binary Tree.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+class Solution:
+    def bottomView(self, root):
+        if root==None:
+            return []
+        ans=[]
+        map={}
+        q=deque()
+        q.append((root, 0))
+        while q:
+            x=q.popleft()
+            line=x[1]
+            node=x[0]
+            map[line]=node.data
+            if node.left:
+                q.append((node.left, line-1))
+            if node.right:
+                q.append((node.right, line+1))
+        for key in sorted(map.keys()):
+            ans.append(map[key])
+        return ans
