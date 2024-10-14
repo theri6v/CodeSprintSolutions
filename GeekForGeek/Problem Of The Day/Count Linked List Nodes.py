592aee1b70d450d7f1a961e5a59057b0870ca83c
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2024-10-14 22:55:53 +0530

    Create Count Linked List Nodes.py

---
+++ GeekForGeek/Problem Of The Day/Count Linked List Nodes.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+'''
+
+#Linked list class
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        '''
+class Solution:
+    # Function to count nodes of a linked list.
+    def getCount(self, head):
+        # code here
+        
+        if not head:
+            return 0
+        
+        return 1 + self.getCount(head.next)
+        
