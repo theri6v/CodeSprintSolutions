0c09913fbcde9b2a292a2ea04be8467e2b4e6a04
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2025-02-18 22:55:37 +0530

    Create K Closest Points to Origin.py

---
+++ GeekForGeek/Problem Of The Day/K Closest Points to Origin.py
@@ -0,0 +1,6 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return sorted(points, key=lambda p: p[0]**2 + p[1]**2)[:k]
+            
+        
+        
