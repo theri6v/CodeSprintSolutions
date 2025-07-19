67e6f5277f932d7ea85c6d7f20e147752962576a
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2025-07-19 22:58:22 +0530

    Create Count Unique Vowel Strings.py

---
+++ GeekForGeek/Problem Of The Day/Count Unique Vowel Strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def vowelCount(self, s):
+        from functools import reduce
+        key = {}
+        vowels = 'aeiou'
+        for i in s:
+            if i in vowels:
+                if i not in key:
+                    key[i] = 1
+                else:
+                    key[i] += 1
+        if len(key.keys()) == 0:
+            return 0
+        key_prod = reduce(lambda x,y: x*y, key.values())
+        fact = reduce(lambda x,y: x*y, [i for i in range(1,len(key.keys())+1)])
+        return fact * key_prod
