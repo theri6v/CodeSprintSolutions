1711164309d9f180276f82abae2358a259d60c70
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2025-07-30 22:43:31 +0530

    Create Subarrays with sum K(rotd).py

---
+++ GeekForGeek/Problem Of The Day/Subarrays with sum K(rotd).py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+class Solution:
+    def cntSubarrays(self, arr, k):
+        dic=defaultdict(int)
+        ans=0
+        sums=0
+        dic[0]+=1
+        for i in arr:
+            sums+=i
+            find=sums-k
+            ans+=dic[find]
+            dic[sums]+=1
+        return ans
