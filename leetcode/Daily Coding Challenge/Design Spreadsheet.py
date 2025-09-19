7c29f67315c19027d97417a2886884d730cb7c34
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2025-09-19 22:35:23 +0530

    Create Design Spreadsheet.py

---
+++ leetcode/Daily Coding Challenge/Design Spreadsheet.py
@@ -0,0 +1,22 @@
+class Spreadsheet:
+  def __init__(self, rows: int) -> None:
+    self.spreadsheet = {}
+
+  def setCell(self, cell: str, value: int) -> None:
+    self.spreadsheet[cell] = value
+
+  def resetCell(self, cell: str) -> None:
+    self.spreadsheet[cell] = 0
+
+  def getValue(self, formula: str) -> int:
+    i = formula.find('+')
+    return self._getToken(formula[1:i]) + self._getToken(formula[i+1:])
+
+  def _getToken(self, token: str) -> int:
+    return int(token) if token[0].isdigit() else self.spreadsheet.get(token, 0)
+
+# Your Spreadsheet object will be instantiated and called as such:
+# obj = Spreadsheet(rows)
+# obj.setCell(cell,value)
+# obj.resetCell(cell)
+# param_3 = obj.getValue(formula)
