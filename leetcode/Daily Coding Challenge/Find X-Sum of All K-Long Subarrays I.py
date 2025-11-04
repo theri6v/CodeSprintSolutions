f7355b23071844136e1c7f4301bbbb2ef4f65d7d
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2025-11-04 22:34:17 +0530

    Add solution for finding X-Sum of K-long subarrays

---
+++ leetcode/Daily Coding Challenge/Find X-Sum of All K-Long Subarrays I.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        n = len(nums)
+        ans = list()
+        for i in range(n - k + 1):
+            cnt = Counter(nums[i : i + k])
+            freq = sorted(cnt.items(), key=lambda item: (-item[1], -item[0]))
+            xsum = sum(key * value for key, value in freq[:x])
+            ans.append(xsum)
+        return ans
