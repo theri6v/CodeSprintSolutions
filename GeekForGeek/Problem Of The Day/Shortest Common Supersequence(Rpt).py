e908474b45a87a88a8be0c25f457e1da7528b8e7
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2025-11-11 22:01:05 +0530

    Add minSuperSeq method to calculate supersequence
    Implement dynamic programming solution for finding the shortest common supersequence length.

---
+++ GeekForGeek/Problem Of The Day/Shortest Common Supersequence(Rpt).py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSuperSeq(self, s1, s2):
+        # code here
+        l1, l2 = len(s1), len(s2)
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+        for i in range(1, l1 + 1):
+            for j in range(1, l2 + 1):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+        return l1 + l2 - dp[-1][-1]
