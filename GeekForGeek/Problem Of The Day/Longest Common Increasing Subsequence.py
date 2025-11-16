a67d8b244820b9b6c4ef52f2a8a70e4a2c9687e3
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2025-11-16 20:42:30 +0530

    Add LCIS function to find longest common increasing subsequence
    Implement Longest Common Increasing Subsequence algorithm.

---
+++ GeekForGeek/Problem Of The Day/Longest Common Increasing Subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def LCIS(self, a, b):
+        n, m = len(a), len(b)
+        dp = [0] * m
+
+        for i in range(n):
+            currentMax = 0
+            for j in range(m):
+
+                if a[i] == b[j]:
+                    dp[j] = currentMax + 1
+
+                elif b[j] < a[i]:
+                    currentMax = max(currentMax, dp[j])
+
+        return max(dp)
