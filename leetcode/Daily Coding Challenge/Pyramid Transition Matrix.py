5083349f8ea656b5343e3813ba8e69f3e193584c
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2025-12-29 22:02:34 +0530

    Create Pyramid Transition Matrix.py

---
+++ leetcode/Daily Coding Challenge/Pyramid Transition Matrix.py
@@ -0,0 +1,33 @@
+class Solution:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        tab = defaultdict(set)
+        for u, v, w in allowed:
+            tab[u, v].add(w)
+
+        def add_neighbor(node):
+            res = ['']
+            for i in range(1, len(node)):
+                eles = tab[(node[i - 1], node[i])]
+                if eles:
+                    res = [a + e for e in eles for a in res]
+                else:
+                    return []
+            return res
+        
+        
+        visited = set()
+
+        def dfs(node):
+            if len(node) == 1:
+                return True
+            if node in visited:
+                return False
+
+            for nxt in add_neighbor(node):
+                if dfs(nxt):
+                    return True
+
+            visited.add(node)
+            return False
+
+        return dfs(bottom)
