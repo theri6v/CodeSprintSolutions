266dd9ac26d24f9e0ace3352c5924cb2a85b6b32
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2026-02-04 22:50:29 +0530

    Implement solution for ant falling problem

---
+++ GeekForGeek/Problem Of The Day/Last Moment Before All Ants Fall Out(rpt).py
@@ -0,0 +1,10 @@
+class Solution:
+    def getLastMoment(self, n, left, right):
+        # If 'left' is not empty, max time is the furthest left ant's position
+        max_left = max(left) if left else 0
+        
+        # If 'right' is not empty, max time is the max(n - pos)
+        max_right = max((n - r for r in right), default=0)
+        
+        # The result is the maximum time any ant takes to fall
+        return max(max_left, max_right)
