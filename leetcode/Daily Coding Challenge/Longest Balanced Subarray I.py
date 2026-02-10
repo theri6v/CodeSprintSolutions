94d77daf359f45a40b27e9d354732f79ce84e12a
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2026-02-10 22:28:22 +0530

    Add solution for longest balanced subarray problem
    Implement longestBalanced method to find the length of the longest balanced subarray.

---
+++ leetcode/Daily Coding Challenge/Longest Balanced Subarray I.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        n=len(nums)
+        seen=set()
+        Len=0
+        for l in range(n):
+            if l>n-Len: break # add this line
+            diff=0
+            for r in range(l, n):
+                x=nums[r]
+                if x not in seen:
+                    diff+=1-(x&1)*2
+                    seen.add(x)
+                if diff==0:
+                    Len=max(Len, r-l+1)
+            seen.clear()
+        return Len
+        
